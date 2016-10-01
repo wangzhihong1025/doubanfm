87c5161b07670a15a0e42d442839a14392472522
wangzhihong <wang>
2016-10-02 03:37:41 +0800

    ul列表及滚动，音乐播放

--- doubanfm.js
+++ doubanfm.js
@@ -9,4 +9,45 @@
     sanjiaochoose.onmouseout = function() {
         xialakuang.className = "hid"
     }
+
+    var audio = document.getElementById('audio');
+    var jindu = document.getElementById('jindu');
+    var jindutiao = document.getElementById('jindutiao');
+    var jixu = document.getElementById('jixu');    
+    jindutiao.onmousedown = function(event) {
+        var length = event.clientX - jindu.offsetLeft;
+        var percent = (length-870) / jindutiao.offsetWidth;
+        jindu.style.width = percent * 210  + "px";
+        audio.currentTime = percent * audio.duration;
+    }
+    jindu.onmousedown = function(event) {
+        var length = event.clientX - jindu.offsetLeft;
+        var percent = (length-870) / jindutiao.offsetWidth;
+        jindu.style.width = percent * 210 +"px";
+        audio.currentTime = percent * audio.duration;
+    }
+    var play = document.getElementById('play');
+    var musicyinying = document.getElementById('musicyinying');
+    var jixu = document.getElementById('jixu');
+    play.onclick = function () {
+    	if (audio.played) {
+            audio.pause();singerphoto
+    	} 
+    	musicyinying.style.display="block";    	
+    }
+    jixu.onclick = function () {
+    	if (audio.paused) {
+            audio.play();
+    	}
+    	musicyinying.style.display="none";   	
+    }
+    var photoyinying = document.getElementById('photoyinying');
+    var chakan = document.getElementById('musicyinying');
+    var singerphoto = document.getElementById('singerphoto');
+    singerphoto.onmouseover = function () { 
+    	photoyinying.style.display="block";    	
+    }
+    photoyinying.onmouseout = function () {
+    	photoyinying.style.display="none";   	
+    }
 }(window))
